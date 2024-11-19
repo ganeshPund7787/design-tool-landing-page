dddc472dde1918608ae129f4975565c341e45e1b
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-19 19:41:06 +0500

    working on Hero

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,14 @@
-import { HTMLAttributes } from "react";
+import { ButtonHTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
 
-const classes = cva("border  h-12 rounded-full bg-green-400 px-6 font-medium", {
+const classes = cva("border h-10 rounded-full bg-green-400 px-5 font-medium", {
     variants: {
         variant: {
             primary: "bg-lime-400 text-neutral-950 border-lime-400",
             secondary: "border-white text-white bg-transparent",
+        },
+        size: {
+            sm: "h-10",
         },
     },
 });
@@ -13,7 +16,8 @@ const classes = cva("border  h-12 rounded-full bg-green-400 px-6 font-medium", {
 export default function Button(
     props: {
         variant: "primary" | "secondary";
-    } & HTMLAttributes<HTMLButtonElement>
+        size?: "sm";
+    } & ButtonHTMLAttributes<HTMLButtonElement>
 ) {
     const { variant, className, ...OtherProps } = props;
 
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,41 @@
+import Button from "@/components/Button";
+
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                        ✨ $7.5M seed round raised
+                    </div>
+                </div>
+                <h1 className="text-6xl font-medium text-center mt-6">
+                    Impactful design, created effortful
+                </h1>
+                <p className="text-center text-xl text-white/50 mt-8">
+                    Design tools shouldn&apos;t slow you down. Layers combines
+                    powerful features with an intuitive interface that keeps you
+                    in your creative flow.
+                </p>
+                <form
+                    action=""
+                    className="flex border border-white/15 rounded-full py-2 mt-8"
+                >
+                    <input
+                        type="email"
+                        placeholder="Enter your email"
+                        className="bg-transparent px-2 border-2"
+                    />
+                    <Button
+                        type="submit"
+                        className="whitespace-nowrap"
+                        size="sm"
+                        variant="primary"
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+            </div>
+        </section>
+    );
 }

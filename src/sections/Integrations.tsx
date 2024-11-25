d624ab6c96ebdf7ac5c531d1ea54bb71dcff023e
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-25 13:30:37 +0530

    create Integrations ui

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -1,12 +1,35 @@
-const integrations = [
-    { name: "Figma", icon: "figma-logo.svg", description: "Figma is a collaborative interface design tool." },
-    { name: "Notion", icon: "notion-logo.svg", description: "Notion is an all-in-one workspace for notes and docs." },
-    { name: "Slack", icon: "slack-logo.svg", description: "Slack is a powerful team communication platform." },
-    { name: "Relume", icon: "relume-logo.svg", description: "Relume is a no-code website builder and design system." },
-    { name: "Framer", icon: "framer-logo.svg", description: "Framer is a professional website prototyping tool." },
-    { name: "GitHub", icon: "github-logo.svg", description: "GitHub is the leading platform for code collaboration." },
-];
+import IntegrationColumn from "@/components/IntegrationColumn";
+import Tag from "@/components/Tag";
+import { integrations } from "@/Data/PageData";
 
 export default function Integrations() {
-    return <div>Integrations</div>;
+    return (
+        <section className="py-24 overflow-hidden">
+            <div className="container">
+                <div className="grid lg:grid-cols-2 items-center lg:gap-16 ">
+                    <div className="">
+                        <Tag>Integration</Tag>
+                        <h2 className="text-6xl font-medium mt-6">
+                            Integrates smoothly with{" "}
+                            <span className="text-lime-400">others</span>
+                        </h2>
+                        <p className="text-white/15 mt-4 text-lg ">
+                            Layers effortlessly integrates with your preferred
+                            tools, making it simple to fit into any workflow and
+                            collaborate across platforms.
+                        </p>
+                    </div>
+                    <div className="">
+                        <div className="h-[400px] lg:h-[800px] mt-8 lg:mt-0 overflow-hidden grid md:grid-cols-2 gap-4 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
+                            <IntegrationColumn integrations={integrations} />
+                            <IntegrationColumn
+                                integrations={integrations.slice().reverse()}
+                                className="hidden md:flex"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }

32cbd6b9e6cdfc003a914e410e3f9c0924a572b7
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-29 22:21:40 +0530

    add callToAction anikmation

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,17 +1,50 @@
+"use client";
+
+import { AnimationPlaybackControls, motion, useAnimate } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+
 export default function CallToAction() {
+    const [isHoverd, setIsHoverd] = useState(false);
+    const animation = useRef<AnimationPlaybackControls>();
+    const [scope, animate] = useAnimate();
+
+    useEffect(() => {
+        animation.current = animate(
+            scope.current,
+            { x: "-50%" },
+            { duration: 30, ease: "linear", repeat: Infinity }
+        );
+    }, []);
+
+    useEffect(() => {
+        if (animation.current) {
+            if (isHoverd) {
+                animation.current.speed = 0.5;
+            } else {
+                animation.current.speed = 1;
+            }
+        }
+    }, [isHoverd]);
     return (
         <section className="py-24">
             <div className="overflow-x-clip p-4 flex">
-                <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
+                <motion.div
+                    ref={scope}
+                    className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium group cursor-pointer"
+                    onMouseEnter={() => setIsHoverd(true)}
+                    onMouseLeave={() => setIsHoverd(false)}
+                >
                     {Array.from({ length: 10 }).map((_, i) => (
                         <div key={i} className="flex items-center gap-16">
                             <span className="text-lime-400 text-7xl">
                                 &#10038;
                             </span>
-                            <span>Try it for free </span>
+                            <span className="group-hover:text-lime-400 group-hover:shadow-2xl">
+                                Try it for free{" "}
+                            </span>
                         </div>
                     ))}
-                </div>
+                </motion.div>
             </div>
         </section>
     );
